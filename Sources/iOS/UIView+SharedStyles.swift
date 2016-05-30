4db04241bcab277fbb7dea6da491ae49b007d674
Yonas Kolb <yonaskolb@users.noreply.github.com>
2016-05-30 13:21:36 +1000

    remove Sugar dependancy

--- Sources/iOS/UIView+SharedStyles.swift
+++ Sources/iOS/UIView+SharedStyles.swift
@@ -1,5 +1,4 @@
 import UIKit
-import Sugar
 
 extension UIView {
 
@@ -48,4 +47,37 @@ extension UIView {
         newValue, objc_AssociationPolicy.OBJC_ASSOCIATION_RETAIN_NONATOMIC)
     }
   }
+}
+
+public struct Swizzler {
+
+    public enum Kind {
+        case Instance
+        case Class
+    }
+
+    public static func swizzle(method: String, cls: AnyClass!, prefix: String = "swizzled", kind: Kind = .Instance) {
+        let originalSelector = Selector(method)
+        let swizzledSelector = Selector("\(prefix)_\(method)")
+
+        let originalMethod = kind == .Instance
+            ? class_getInstanceMethod(cls, originalSelector)
+            : class_getClassMethod(cls, originalSelector)
+
+        let swizzledMethod = kind == .Instance
+            ? class_getInstanceMethod(cls, swizzledSelector)
+            : class_getClassMethod(cls, swizzledSelector)
+
+        let didAddMethod = class_addMethod(cls, originalSelector,
+                                           method_getImplementation(swizzledMethod),
+                                           method_getTypeEncoding(swizzledMethod))
+
+        if didAddMethod {
+            class_replaceMethod(cls, swizzledSelector,
+                                method_getImplementation(originalMethod),
+                                method_getTypeEncoding(originalMethod))
+        } else {
+            method_exchangeImplementations(originalMethod, swizzledMethod)
+        }
+    }
 }
